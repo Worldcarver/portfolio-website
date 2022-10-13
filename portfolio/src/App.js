2034e0a09e1d6f3b8bf67799dcee55c8a2f1ef2b
ian <ian.murphy92@gmail.com>
2022-10-13 15:45:18 -0400

    added github link

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,8 +8,8 @@ function App() {
     return (
 
         <>
-        
-        <img src="cdn.discordapp.com/attachments/1007749002248327178/1029808666783780984/tarkwebside.jpg"/>
+        <p a = "https://github.com/Worldcarver/Capstone">Github</p>
+        <img src="cdn.discordapp.com/attachments/1007749002248327178/1029808666783780984/tarkwebside.jpg" a = "https://github.com/Worldcarver/Capstone"/>
         
         
         </>
